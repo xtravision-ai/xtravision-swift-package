73a3f3e4b15d8df7e6261df7ae0beb74cb99468b
riddhixtraining <riddhi.shah@xtraininglive.com>
2022-11-18 13:29:31 +0530

    Updated checksum for Live url

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "XtraVisionAI",
             url: "https://github.com/xtravision-ai/xtravision-swift-framework/releases/download/v1.0.1/XtraVisionAI.xcframework.zip",
-            checksum: "a1ba14548e6c45ac8d018e7a20ab19577c4f920ce062291e63c9a018e44e69b0"
+            checksum: "fed59cd3b01b7052b858b1a361dd005f83d5ce6c1188d16417904562afd3204c"
         )
     ]
 )
